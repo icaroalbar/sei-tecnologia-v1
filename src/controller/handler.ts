dfe2c2f3f11ff08524955ca2df756dd945e869e7
Icaro Albar <icaro.albar@live.com>
2025-05-13 14:53:14 -0300

    :sparkles: feat: endpoint send documents at s3

--- src/controller/handler.ts
+++ src/controller/handler.ts
@@ -1,17 +1,26 @@
 import { APIGatewayProxyEvent } from "aws-lambda";
 import { formatJSONResponse } from "../libs/api-gateway";
 import multipart from "lambda-multipart-parser";
-// import { S3Client } from "@aws-sdk/client-s3";
+import { PutObjectCommand, S3Client } from "@aws-sdk/client-s3";
 
 const handler = async (event: APIGatewayProxyEvent) => {
   const parsedEvent = await multipart.parse(event);
-
-  // const client = new S3Client(process.env.AWS_BUCKET_REGION);
 
   try {
     if (!parsedEvent.files || parsedEvent.files.length === 0) {
       throw new Error("Arquivo não encontrado!");
     }
+
+    const client = new S3Client(process.env.AWS_BUCKET_STORE);
+
+    const input = {
+      Body: parsedEvent.files[0].content,
+      Bucket: process.env.AWS_BUCKET_STORE,
+      Key: parsedEvent.files[0].filename,
+    };
+
+    const command = new PutObjectCommand(input);
+    await client.send(command);
 
     console.log("Arquivo recebido:", parsedEvent.files[0]);
 
