72120b056fdcbaa126bc51a274b423aa0175d874
Icaro Albar <icaro.albar@live.com>
2025-05-14 21:10:30 -0300

    :boom: fix: function textract resolved

--- src/functions/textract/handler.ts
+++ src/functions/textract/handler.ts
@@ -1,32 +1,48 @@
 import {
-  AnalyzeDocumentCommand,
-  AnalyzeDocumentRequest,
+  StartDocumentTextDetectionCommand,
+  GetDocumentTextDetectionCommand,
   TextractClient,
 } from "@aws-sdk/client-textract";
 
+const client = new TextractClient({ region: process.env.AWS_REGION });
+
 const handler = async (event) => {
-  const client = new TextractClient({ region: process.env.AWS_REGION });
+  const bucket = process.env.AWS_BUCKET_STORE;
+  const documentName = event.Records[0].s3.object.key;
 
-  console.log("Nome do documento", event.Records[0].s3.object.key);
-
-  try {
-    const input: AnalyzeDocumentRequest = {
-      Document: {
-        S3Object: {
-          Bucket: process.env.AWS_BUCKET_STORE,
-          Name: event.Records[0].s3.object.key,
-        },
+  const startCommand = new StartDocumentTextDetectionCommand({
+    DocumentLocation: {
+      S3Object: {
+        Bucket: bucket,
+        Name: documentName,
       },
-      FeatureTypes: ["FORMS"],
-    };
+    },
+  });
 
-    const command = new AnalyzeDocumentCommand(input);
-    const response = await client.send(command);
+  const startResponse = await client.send(startCommand);
+  const jobId = startResponse.JobId;
+  console.log("Processando arquivo:", jobId);
 
-    console.log("Resultado:", JSON.stringify(response, null, 2));
-  } catch (error) {
-    console.error("Erro ao ler objeto:", error);
-    throw error;
+  let status = "IN_PROGRESS";
+  let response;
+  while (status === "IN_PROGRESS") {
+    await new Promise((res) => setTimeout(res, 5000));
+
+    const getCommand = new GetDocumentTextDetectionCommand({ JobId: jobId });
+    response = await client.send(getCommand);
+    status = response.JobStatus;
+    console.log("Status:", status);
+  }
+
+  if (status === "SUCCEEDED") {
+    const lines = response.Blocks?.filter(
+      (block) => block.BlockType === "LINE"
+    ).map((line) => line.Text);
+
+    console.log("Textos extraídos:");
+    console.log(lines.join("\n"));
+  } else {
+    throw new Error("Análise falhou");
   }
 };
 
