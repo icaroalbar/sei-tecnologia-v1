dab59191fe5ebbd2d77bc6aacbcba14d8e9cc90a
Icaro Albar <icaro.albar@live.com>
2025-05-28 16:06:32 -0300

    :bug: fix: deploy api processed documents

--- src/functions/index.ts
+++ src/functions/index.ts
@@ -1,3 +1,3 @@
 export { default as saveBucket } from "./save-bucket";
-export { default as processedDocuments } from "./ processed_documents";
+export { default as processedDocuments } from "./processed_documents";
 export { default as resumeDocument } from "./resume-document";
